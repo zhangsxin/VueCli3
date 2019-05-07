09a2b44c88513d343dd9eb2cd98b12b5e0c5737a
wangwenjian <man@wwenj.com>
2019-05-07 20:41:00 +0800

    V1.0

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style>
+@import 'style/reset.css';
+#app{
+  height: 100vh;
+  overflow: auto;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    home
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+export default {
+  name: 'home',
+  mounted () {
+    this.init()
+    console.log(this.id)
+  },
+  computed: {
+    ...mapGetters(['id'])
+  },
+  methods: {
+    init () {
+      this.setId('wangwenj')
+      let params = {
+        name: 'wang',
+        age: 'wen'
+      }
+      this.$http.get('http://yapi.demo.qunar.com/mock/63722/home', params).then(res => {
+        console.log(res)
+      })
+      this.$http.post('http://yapi.demo.qunar.com/mock/63722/about', params).then(res => {
+        console.log(res)
+      })
+    },
+    ...mapActions(['setId'])
+  }
+}
+</script>
